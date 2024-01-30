7792c8bda1140c7f37244697f4168cd3bbc1c13a
MJ Belleville <villebellez@outlook.com>
2024-01-30 12:34:47 -0500

    Update main.cpp
    Created main() program loop with display and input screens

--- main.cpp
+++ main.cpp
@@ -6,21 +6,83 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include "Investments.h"
 using namespace std;
 
+struct Input {
+	double deposit;
+	double monthly;
+	double rate;
+	int years;
+};
+
+void displayScreen() {
+	cout << setfill('*') << setw(38) << "" << endl;
+	cout << "* * * * * * * DATA INPUT * * * * * * *" << endl;
+	cout << setfill('*') << setw(38) << "" << endl;
+	cout << "Initial Investment Amount: " << endl;
+	cout << "Monthly Deposit: " << endl;
+	cout << "Annual Interest: " << endl;
+	cout << "Number of years: " << endl;
+	cout << endl;
+}
+
+Input inputScreen() {
+	double deposit;
+	double monthly;
+	double rate;
+	int years;
+	Input i;
+
+	cout << setfill('*') << setw(38) << "" << endl;
+	cout << "* * * * * * * DATA INPUT * * * * * * *" << endl;
+	cout << setfill('*') << setw(38) << "" << endl;
+
+	cout << "Initial Investment Amount: $";
+	cin >> deposit;
+
+	cout << "Monthly Deposit: $";
+	cin >> monthly;
+
+	cout << "Annual Interest: %";
+	cin >> rate;
+
+	cout << "Number of years: ";
+	cin >> years;
+
+	i.deposit = deposit;
+	i.monthly = monthly;
+	i.rate = rate;
+	i.years = years;
+
+	return i;
+}
+
 int main() {
+	string loop = "y";
 
-	// Testing
-	double deposit = 0.0;
-	double monthly = 0.0;
-	double rate = 0.0;
-	int numYears = 0;
+	displayScreen();
 
-	auto account = Investments(deposit, monthly, rate, numYears);
+	while (loop == "y") {
+		system("pause");
 
-	account.yesMonthlyDeposit();
-	account.noMonthlyDeposit();
+		system("cls");
+		Input i = inputScreen();
+		system("pause");
 
+		auto account = Investments(i.deposit, i.monthly, i.rate, i.years);
+
+		account.yesMonthlyDeposit();
+		account.noMonthlyDeposit();
+
+		cout << endl << "Would you like to enter different values? (y/n): ";
+		cin >> loop;
+
+		// TODO: Error catching for anything other than y/n
+		if (loop != "y") {
+			break;
+		}
+	}
 	return 0;
 }
